8516a7cba3576636ea7b146611d3bb8e4fb9c8e8
Tapash Nath <debnathtapash56@gmail.com>
2024-07-18 14:42:51 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 
 module.exports = {
        images: {
-         domains: ['backend-tendoni-backend.ffbufe.easypanel.host', ''],
+         domains: ['img.freepik.com','backend-tendoni-backend.ffbufe.easypanel.host'],
        },
      };
